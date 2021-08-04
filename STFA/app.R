3ef05a2df481630bf36c759938d3bedafc96b563
syedahmadzaki <syed.ahmadzaki@yahoo.com.sg>
2021-08-05 00:15:41 +0800

    Initial look

--- STFA/app.R
+++ STFA/app.R
@@ -1,39 +1,59 @@
-library(shiny)
+# Install and load key R libraries
+
+ipak <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)) 
+        install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("shiny","shinydashboard","readr","tidyverse","shinythemes","readxl")
+ipak(packages)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <-  navbarPage(title = "VAST Mini Challenge 2", selected = " Intro", collapsible = TRUE, inverse = FALSE, theme = shinytheme("flatly"),
+                  
+                    # Intro Tab - Mainly a write-up of our project.
+                  
+                    tabPanel(" Intro",icon = icon("chalkboard-teacher"),
+                             tags$div(
+                                 tags$h4("ABOUT"),
+                                 "Write an intro part one here.",
+                                 tags$br(), tags$br(),
+                                 "Write intro part two here."
+                             )),
+                         
+                    # User Guide Tab - Explains how to use our Shiny app.
+                           
+                    tabPanel(" User Guide",icon = icon("question-circle")),
+                                    
+                    # Exploring GPS Tab - Explores the GPS data and locations on a map.
+                             
+                    tabPanel(" Exploring GPS",icon = icon("map-marked-alt")),
+                                      
+                    # Parallel Coords Tab - Exploring data using a parallel coords tab.
+                                      
+                    tabPanel(" Parallel Coords",icon = icon("wave-square")),
+                                               
+                    # Event Timeline Tab - Exploring happenings via an event timeline.
+                                               
+                    tabPanel(" Event Timeline",icon = icon("stream")),
+                                                        
+                    # Network Graph Tab - Understanding the linkages between locations at certain junctures and people.
+                                                        
+                    tabPanel(" Network Graph",icon = icon("project-diagram")),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+                    # Stats Graph Tab - Explores statistical analysis on our data.
+                             
+                    tabPanel(" Stats Graph",icon = icon("signal"))
+                                                        
+                           )
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 # Run the application 
