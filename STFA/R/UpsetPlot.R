5a8ebc85fe5cbd1fbf48ee90947d9b7ad40837cc
kpokp <58965828+kpokp@users.noreply.github.com>
2021-08-09 00:07:52 +0800

    kelly draft 4

--- STFA/R/UpsetPlot.R
+++ STFA/R/UpsetPlot.R
@@ -1,3 +1,10 @@
+library(shiny)
+library(tidyverse)
+library(plyr)
+library(ggplot2)
+library(UpSetR)
+
+# ----------------------------------------------------------------------------------------------------------------------
 # Import csv data function 
 
 csvFileUI <- function(id, label = "CSV file") {
@@ -15,30 +22,38 @@ csvFileUI <- function(id, label = "CSV file") {
     
     # Select dataset from VAST challenge
     radioButtons(
-      ns('cc_data'),
+      NS(id,'choose_data'),
       label = ('Select dataset'),
       choices = c(
-        'Credit Card dataset',
-        'Upload own dataset')
+        'Credit Card dataset' = "cc_data",
+        'Upload own dataset' = "user_data")
     ),
     
     # Prompt user to upload csv file
-    fileInput(ns("file"),
+    fileInput(NS(id,"file"),
               label = "Upload own dataset (csv format)",
               buttonLabel = "Upload file"),
     
     # Users to indicate if file has header
-    checkboxInput(ns('heading'), label = 'Has Header', TRUE),
+    checkboxInput(NS(id,'heading'), label = 'Has Header', TRUE),
 
     # Users to indicate type of quote
-    selectInput(ns("quote"), "Quote", c(
+    selectInput(NS(id,"quote"), "Quote", c(
       "None" = "",
       "Double quote" = "\"",
       "Single quote" = "'"
     )),
     
     # Users to indicate no. of rows to preview
-    numericInput(ns("rows"), "Rows to preview", 10, min = 1)
+    numericInput(ns("rows"), "Rows to preview", 10, min = 1),
+    
+    br(),
+    
+    # Users to confirm using dataset
+    actionButton("confirm","Plot"),
+    
+    br(),
+    br()
   )
 }
 
@@ -52,6 +67,7 @@ csvFileServer <- function(id, stringsAsFactors) {
         input$file
       })
       
+      # Change input file into dataframe
       dataframe <- reactive({
         userFile_csv <- read.csv(userFile()$datapath,
                  header = input$heading,
@@ -66,14 +82,194 @@ csvFileServer <- function(id, stringsAsFactors) {
       })
       
       return(dataframe)
+      
+      # Change dataframe into a reactive input for other modules
+      userFile_csv = reactive(input$userFile_csv)
+    
     }
   )    
 }
 
-# Clean data function
-
+# ----------------------------------------------------------------------------------------------------------------------
 # Plot chart function
 
+UpSetPlotUI <- function(id, label = "UpSet Plot") {
+  
+  ns <- NS(id)
+  tagList(
+    br(),
+    
+    sliderInput(
+      NS(id,"setsize"),
+      label = ("Select no. of sets"),
+      value = 5,
+      min = 2,
+      max = 50),
+    
+    fluidRow(
+      column(10,plotOutput(NS(id,"upset"))),
+    
+      column(10,dataTableOutput(NS(id,"upset_table"))), 
+    )
+  )
+}
+
+UpSetPlotServer <- function(id) {
+  moduleServer(
+    id,
+    function(input, output, session, ImProxy) {
+      
+      # Allow users to view all the sets available for selection
+      FindStartEnd <- function(data){
+        startend <- c()
+        for(i in 1:ncol(data)){
+          column <- data[, i]
+          column <- (levels(factor(column)))
+          if((column[1] == "0") && (column[2] == "1" && (length(column) == 2))){
+            startend[1] <- i
+            break
+          }
+          else{
+            next
+          }
+        }
+        for(i in ncol(data):1){
+          column <- data[ ,i]
+          column <- (levels(factor(column)))
+          if((column[1] == "0") && (column[2] == "1") && (length(column) == 2)){
+            startend[2] <- i
+            break
+          }
+          else{
+            next
+          }
+        }
+        return(startend)
+      }
+      
+      startEnd <- reactive({
+        startEnd <- FindStartEnd(My_data())
+      })
+      
+      Specific_sets <- reactive({
+        Specific_sets <- as.character(c(input$Select))
+      })
+      
+      output$sets <- renderUI({
+        if(is.null(My_data()) == T){
+          sets <-  selectInput(NS(id,"Select"), h6("Select at least two sets : "),
+                               choices = NULL,
+                               multiple=TRUE, selectize=TRUE, selected = Specific_sets())
+        }
+        else{
+          data <- My_data()[startEnd()[1]:startEnd()[2]]
+          topfive <- colSums(data)
+          topfive <- as.character(head(names(topfive[order(topfive, decreasing = T)]), 5))
+          sets <- selectInput(NS(id,'Select'), h6("Select specific sets : "),
+                              choices = as.character(colnames(My_data()[ , startEnd()[1]:startEnd()[2]])),
+                              multiple=TRUE, selectize=TRUE, selected = topfive)
+        }
+        return(sets)
+      })
+      
+      # temp >> attempt to try plotting using internal dataset
+      cc_csv <- read.csv("cc_pivot.csv", header=T, sep="," ) %>%
+        dplyr::select(-X)
+      
+      output$upset_table <- renderDataTable(head(cc_csv,10),
+                                            options = list(scrollX = TRUE))
+      
+      # Show graph using render Image
+      output$upset <- renderPlot({
+        
+        # Plot graph after users click on Plot button
+        input$confirm
+        
+        # Unable to read from other modules
+        #UpSetR::upset(data = ImProxy$userFile_csv, nsets = 6, order.by = "freq")
+
+        # Tried reading from own folder but cannot recognize filename
+        #UpSetR::upset(data = cc_csv, 
+                      #nsets = input$setsize, 
+                      #nintersects = input$nintersections, 
+                      #order.by = input$order,
+                      #decreasing = input$decreasing,
+                      #empty.intersections = input$empty
+                      #)
+        
+        UpSetR::upset(data = cc_csv, 
+                      nsets = input$setsize, 
+                      nintersects = 50, 
+                      order.by = "freq",
+                      decreasing = TRUE,
+                      empty.intersections = "off"
+        )
+      })
+      
+    }
+)}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Plot chart settings
+# Can have 2 UI module syncing with 1 Server Module?
+
+SettingsUI <- function(id, label = "Settings") {
+  
+  ns <- NS(id)
+  tagList(
+    
+    tabsetPanel(
+      tabPanel(
+        'Setting',
+        br(),
+        
+        # Show the sets from the dataset for multiple selection
+        htmlOutput("sets"),
+        
+        # No. of intersections
+        sliderInput(
+          NS(id,"setsize"),
+          label = ("Select no. of sets"),
+          value = 5,
+          min = 2,
+          max = 50),
+        
+        # No. of intersections
+        numericInput(
+          NS(id,"nintersections"),
+          label = ("Limit no. of intersections"),
+          value = 50,
+          min = 1,
+          max = 60),
+        
+        # Order by degree or frequency
+        selectInput(
+          NS(id,"order"),
+          label = ("Order intersections by"),
+          choices = list("Degree" = "degree",
+                         "Frequency" = "freq"),
+          selected = "freq"),
+        
+        # Order descending or ascending
+        selectInput(
+          NS(id,"decreasing"),
+          label = ("Increasing/Decreasing"),
+          choices = list("Increasing" = FALSE,
+                         "Decreasing" = TRUE),
+          selected = TRUE),
+        
+        # To show empty intersections
+        checkboxInput(NS(id,"empty"), 
+                      label = "Show empty intersections", 
+                      value = FALSE),
+        
+      ),
+      tabPanel("Format"
+      )
+    )
+    
+  )
+}
 
 # ----------------------------------------------------------------------------------------------------------------------
 # To port over to main app later
@@ -81,12 +277,15 @@ csvFileServer <- function(id, stringsAsFactors) {
 ui <- fluidPage(
   
   # Application title
-  titlePanel("UpSet Plot for Co-Location Investigation"),
+  titlePanel("UpSet Plot - View Locations with Same Visitors"),
   
   sidebarLayout(
     
     # Sidebar for basic and advanced settings for Upset Plot
-    sidebarPanel(),
+    sidebarPanel(width = 2,
+      h4(HTML("UpSet Plot Settings")),
+      SettingsUI("settings")
+    ),
     
     # Show tab panels of file import, data cleaning and plot 
     mainPanel(
@@ -98,11 +297,9 @@ ui <- fluidPage(
                  csvFileUI("datafile", "User data (.csv format)"),
                  dataTableOutput("table")),
         
-        # Step 2 - User to clean data if necessary
-        tabPanel("Clean Data"),
-        
-        # Step 3 - User to plot chart
-        tabPanel("UpSet Plot")
+        # Step 2 - User to plot chart
+        tabPanel("UpSet Plot", 
+                 UpSetPlotUI("upset"))
       )
     )
   )
@@ -110,6 +307,7 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  # Ingest uploaded dataset
   datafile <- csvFileServer("datafile", stringsAsFactors = FALSE)
   
   # Preview imported data set
@@ -117,9 +315,8 @@ server <- function(input, output, session) {
     datafile()
   })
   
-  # Preview clean data set
-  
   # Upset plot
+  UpSetPlotServer("upset")
   
 }
 
--- STFA/R/scrap.R
+++ STFA/R/scrap.R
@@ -1,7 +1,7 @@
 library(UpSetR)
 library(tidyverse)
 
-cc_data <- read_csv("data/cc_data.csv")
+cc_data1 <- read.csv("data/cc_data.csv")
 
 cc_data[grep("Katerina", cc_data$location),2] <- "Katerina's Cafe"
 
@@ -12,9 +12,22 @@ cc_data_2 <- cc_data %>%
 cc_pivot <- cc_data_2 %>% 
   pivot_wider(last4ccnum, names_from = location, values_from = present, values_fill = 0) %>%
   mutate(across(everything(), ~replace_na(.x, 0))) %>%
-  lapply(as.integer) %>%
-  glimpse()
+  mutate_all(as.integer) %>%
+  as.data.frame()
+  
+rownames(cc_pivot) <- NULL
 
-as.data.frame(cc_pivot)
+UpSetR::upset(cc_pivot, nsets = 6, order.by = "freq")
 
-UpSetR::upset(cc_pivot)
+#write.csv(cc_pivot,'cc_pivot.csv')
+
+cc_csv <- read.csv("cc_pivot.csv", header=T, sep="," ) %>%
+  dplyr::select(-X)
+
+#UpSetR::upset(cc_csv)
+
+UpSetR::upset(data = cc_pivot, 
+              nsets = 6, 
+              nintersects = 40, 
+              order.by = "freq",
+              decreasing = TRUE)
